3aeae662df2eb4af86aa6d4216371e91db083bb9
Guga Sobis <gugasobis@gmail.com>
2015-07-29 09:17:42 -0300

    Projeto 2ª fase

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,35 @@
 |
 */
 
-$factory->define(Curso\User::class, function ($faker) {
+$factory->define(Curso\Models\User::class, function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Curso\Models\Client::class, function ($faker) {
+    return [
+        'name' => $faker->name,
+        'responsible' => $faker->name,
+        'email' => $faker->email,
+        'phone' => $faker->str_random(10),
+        'address' => $faker->name,
+        'obs' => $faker->name
+    ];
+});
+
+$factory->define(Curso\Models\Project::class, function ($faker) {
+    return [
+        'owner_id' => $faker->str_random(10),
+        'client_id' => $faker->str_random(10),
+        'name' => $faker->name,
+        'description' => $faker->name,
+        'progress' => $faker->email,
+        'status' => $faker->name,
+        'address' => $faker->name,
+        'due_date' => $faker->date()
     ];
 });
